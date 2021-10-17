762fccb9c8cd632dd0922c30bac893527000b4c3
himanshusahu99 <himanshusahu212456@gmail.com>
2021-10-17 23:04:33 +0530

    added sidebar

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import '../App.css';
 
+const year = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+
 export default function Aside() {
      return (
           <> 
           <div className="aside-outer">
-               <h1>awfd  </h1>
-               {/* <h5>awfd  </h5> */}
+               <h3>Discover Options</h3>
+               <div className="aside-select">
+               <label > TYPE </label> <br/>
+               <select>
+                    <option value="movies">Movies</option>
+                    <option value="tvseries">TV series</option>
+               </select>
+               <br/>
+               <br/>
+               <label > GENRE </label> <br/>
+               <select>
+                    <option value="Actions">Actions</option>
+                    <option value="Drama">Drama</option>
+                    <option value="Comedy">Comedy</option>
+                    <option value="Thriller">Thriller</option>
+               </select> <br/>
+
+               <label > YEAR </label> 
+               <select>
+                   {
+                        year.map(element=><option>{element}</option>)
+                        
+                   }
+               </select> 
+               ---
+               <select>
+                   {
+                        year.map(element=><option>{element}</option>)
+                        
+                   }
+               </select> 
+
+               </div>
           </div>
            
           </> 
